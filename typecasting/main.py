d8f7c5d970e5153d0733ae1be476f8fb9b42b594
Eshma-sum <punkusum76@gmail.com>
2026-02-02 08:09:10 +0545

    updated files

---
+++ typecasting/main.py
@@ -0,0 +1,7 @@
+# what is typecasting?
+# it is the process of converting a variable from one datatype to another.
+
+# type() : used to get the datatype of the variable
+# int() : used to convert a variable to integer
+# str() : used to convert a variable to string
+# float() : used to convert a variable to float
