8d56b4c79095ea03ffe24a0be72e1963664ac03a
Eshma-sum <punkusum76@gmail.com>
2026-02-02 07:19:08 +0545

    added new files

---
+++ variables/main.py
@@ -0,0 +1,18 @@
+name = 'ram'
+print(name)
+
+a,b=1,2
+print(a,b)
+
+a,b,c = 1,2,3
+print(a,b,c)
+
+# array
+a,*b,c = 1,2,3,4,5,6,7,8,9
+print(a,b,c)
+
+a,b,*c = 1,2,3,4,5,6,7,8,9
+print(a,b,c)
+
+*a,b,c = 1,2,3,4,5,6,7,8,9
+print(a,b,c)
