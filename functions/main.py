22fb2e1a03af8df5dd4e1175ae668ab1f8a4db6e
Eshma-sum <punkusum76@gmail.com>
2026-02-03 22:36:39 +0545

    updated function file

--- functions/main.py
+++ functions/main.py
@@ -17,8 +17,10 @@
 # *args
 # **kargs
 
+
+
 # function scope: 
-# 1. local scope: inside the function, can only be accessed inside of the function.
+# 1. local scope: inside the function, can only be accessed inside of the function block.
 # 2. global scope: outside of function, can be accessed by any
 
 # global:
@@ -32,3 +34,37 @@
 #     print(x)
 # print(x)   -> this wont work as the x variabl is local; inside the function
 # info()
+
+# x=10
+# def info():
+#     x=50
+#     print(x)
+# print(x)
+# info()
+
+
+
+# x=10
+# def info():                #OUTPUT:
+#     a=x+50                 #60
+#     print(a)               #10
+# info()
+# print(x)
+
+
+# x=10
+# def info():                #OUTPUT: shows error that the code is not able to identify whether
+#     x=x+50                 the x is local or global
+#     print(x)               
+# info()
+# print(x)
+
+
+# we can change the local variable into global by the following way:
+# x=10
+# def info():
+#     global x
+#     x=x+50
+#     print(x)
+# info()
+# print(x)           # now this print will have 60 as the x variable inside the function block was considered global
