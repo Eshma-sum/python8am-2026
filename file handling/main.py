825bf091d09f822ded88f749d8e282ed571160a3
Eshma-sum <punkusum76@gmail.com>
2026-02-06 16:09:41 +0545

    code added

--- file handling/main.py
+++ file handling/main.py
@@ -12,17 +12,32 @@
 # email = input("enter your email: ")
 # address = input("enter your address: ")
 # obj.write(f"{name}, {email}, {address}\n")
+# print(obj.read())
+# print(obj.readline())
+# print(obj.readlines())
+# obj.close()
 
 
-obj = open("file handling/students.txt","a")
-name = input("enter your name: ")
-maths = int(input("enter maths marks: "))
-science = int(input("enter science marks: "))
-english = int(input("enter english marks: "))
-java = int(input("enter java marks: "))
-python = int(input("enter python marks: "))
+# obj = open("file handling/students.txt","a")
+# name = input("enter your name: ")
+# maths = int(input("enter maths marks: "))
+# science = int(input("enter science marks: "))
+# english = int(input("enter english marks: "))
+# java = int(input("enter java marks: "))
+# python = int(input("enter python marks: "))
 
-total = maths+science+english+java+python
-percentage = total/5
+# total = maths+science+english+java+python
+# percentage = total/5
 
-obj.write(f"{name} ,{maths} ,{science} ,{english} ,{java} ,{python} ,{total} ,{percentage}\n")
+# obj.write(f"name: {name},\nSubjects: {maths} ,{science} ,{english} ,{java} ,{python}, \n{total} ,{percentage}\n")
+# obj.close()
+
+
+
+
+obj = open("file handling/login.txt","w")
+obj.close()
+
+
+# regex
+# tkinter module
