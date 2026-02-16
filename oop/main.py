25dc006c8b542f279cbc5c1c5b89b62cde7dac76
Eshma-sum <punkusum76@gmail.com>
2026-02-16 08:04:05 +0545

    updated oop file

---
+++ oop/main.py
@@ -0,0 +1,15 @@
+#  what is oop
+# OOP is abject oriented programming, where in place of writing functions and variables we use classes and objects.
+# it help to have our code DRY: DONT REPEAT YOURSELF.
+
+# what is class,object,properties and methods, inheritance, 
+# Class is like a blueprint, and object is like
+class MyClass:
+    x = 5
+p = MyClass()
+print(p.x)
+
+# database = mysql
+
+
+# import jason
