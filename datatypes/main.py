ccc6f8134f2c86d195d73045381ff3efe2d9fa3b
Eshma-sum <punkusum76@gmail.com>
2026-02-16 08:27:35 +0545

    updated datatyes file

--- datatypes/main.py
+++ datatypes/main.py
@@ -3,7 +3,20 @@
 # two types of datatype: 
 # 1.primitive: int,floot,str,bool,complex,none
 # 2.non-primitive: list,tuple,set,dict
-
-
 # PRIMITIVE DATA TYPES: They are the ones that have only one value.
 # NON-PRIMITIVE DATA TYPES: They are the ones that have multiple values.
+
+
+# a = 4.123456
+# print(f"{a:.2f}")  
+''' when you want to have the value print with only 2 digit after the point thus we get 4.12 as the output and it also round up
+say  you have a = 4.567812 than you will get 4.57'''
+
+# complex data type: it refers to imaginary numbers where we use j/J.
+# a= 5j
+# print(type(a))
+
+b= 7+6J
+# here 7 is the real number and 6 is the imaginary number
+print(b.real)
+print(b.imag)
